0ae4c47c9a5ccc8103eee5f74656546fc5cc6872
Abdo Tareq <abdotarekfathy@gmail.com>
2020-04-15 15:18:54 +0200

    ... sol 06

--- app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -16,3 +16,40 @@
  */
 
 package com.example.android.devbyteviewer.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import com.example.android.devbyteviewer.database.DatabaseVideo
+import com.example.android.devbyteviewer.database.VideosDatabase
+import com.example.android.devbyteviewer.database.asDomainModel
+import com.example.android.devbyteviewer.domain.Video
+import com.example.android.devbyteviewer.network.Network
+import com.example.android.devbyteviewer.network.asDatabaseModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+/**
+ * A Repository is just a regular class that has one (or more) methods that load data
+ * without specifying the data source as part of the main API
+ *
+ * Even though the repository module looks unnecessary,
+ * it serves an important purpose: it abstracts the data sources from the rest of the app.
+ *
+ * Now, our [DevByteViewModel] doesn't know how the data is fetched,
+ * so we can provide the view model with data obtained from several different data-fetching implementations.
+ * */
+class VideosRepository(private val database: VideosDatabase) {
+
+    val videos: LiveData<List<Video>> = Transformations.map(database.videoDao.getVideos()) {
+        it.asDomainModel()
+    }
+
+    suspend fun refreshVideos() {
+        withContext(Dispatchers.IO) {
+            val playlist = Network.devbytes.getPlaylist().await()
+            // * -> It allows you to pass in an array to a function that expects
+            database.videoDao.insertAll(* playlist.asDatabaseModel())
+        }
+    }
+
+}
